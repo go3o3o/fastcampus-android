d127290835219909bd55ed98151520613c250022
김호연 <hoyeon.kim2773@gmail.com>
2021-11-18 20:43:20 +0900

    안드로이드앱 > 택배 조회 서비스

--- 06/app/src/main/java/com/example/aop_part5_chapter06/work/AppWorkerFactory.kt
+++ 06/app/src/main/java/com/example/aop_part5_chapter06/work/AppWorkerFactory.kt
@@ -1,5 +1,15 @@
 package com.example.aop_part5_chapter06.work
 
+import androidx.work.DelegatingWorkerFactory
+import com.example.aop_part5_chapter06.data.repository.TrackingItemRepository
+import kotlinx.coroutines.CoroutineDispatcher
+
 class AppWorkerFactory(
-) {
+    trackingItemRepository: TrackingItemRepository,
+    dispatcher: CoroutineDispatcher
+): DelegatingWorkerFactory() {
+
+    init {
+        addFactory(TrackingCheckWorkerFactory(trackingItemRepository, dispatcher))
+    }
 }
