608f26c0bbff06440222143bd21a3568e106f050
김호연 <hoyeon.kim2773@gmail.com>
2021-11-22 21:28:50 +0900

    안드로이드앱 > 택배 조회 서비스

--- 06/app/src/main/java/com/example/aop_part5_chapter06/presentation/addtrackingitem/AddTrackingItemFragment.kt
+++ 06/app/src/main/java/com/example/aop_part5_chapter06/presentation/addtrackingitem/AddTrackingItemFragment.kt
@@ -3,6 +3,5 @@ package com.example.aop_part5_chapter06.presentation.addtrackingitem
 import org.koin.android.scope.ScopeFragment
 
 class AddTrackingItemFragment: ScopeFragment(), AddTrackingItemsContract.View {
-
-
+    
 }
