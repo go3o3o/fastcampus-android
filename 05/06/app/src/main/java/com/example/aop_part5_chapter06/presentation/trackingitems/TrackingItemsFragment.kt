11789460cab465dca57412d486e15baa1fe12d8c
yonikim <hoyeon.kim2773@gmail.com>
2021-12-01 15:19:03 +0900

    안드로이드앱 > 택배 조회 서비스

--- 05/06/app/src/main/java/com/example/aop_part5_chapter06/presentation/trackingitems/TrackingItemsFragment.kt
+++ 05/06/app/src/main/java/com/example/aop_part5_chapter06/presentation/trackingitems/TrackingItemsFragment.kt
@@ -2,4 +2,77 @@ package com.example.aop_part5_chapter06.presentation.trackingitems
 
 class TrackingItemsFragment: ScopeFragment(), TrackingItemContract.View {
     override val presenter: TrackingItemsContract.Presenter by inject()
+
+    private var binding: FragmentTrackingItemsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentTrackingItemsBinding.inflate(inflater)
+        .also { binding = it }
+        .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        bindView()
+        presenter.onViewCreated()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        presenter.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.onDestroy()
+    }
+
+    override fun showLoadingIndicator() {
+        binding?.progressBar?.toVisible()
+    }
+
+    override fun hideLoadingIndicator() {
+        binding?.progressBar?.toGone()
+        binding?.refreshLayout?.isRefreshing = false
+    }
+
+    override fun showNoDataDescription() {
+        binding?.refreshLayout?.toInvisible()
+        binding?.noDataContainer?.toVisible()
+    }
+
+    override fun showTrackingItemInformation(trackingItemInformation: List<Pair<TrackingItem, TrackingInformation>>) {
+        binding?.refreshLayout?.toVisible()
+        binding?.noDataContainer?.toGone()
+        (binding?.recyclerView?.adapter as? TrackingItemsAdapter)?.apply {
+            this.data = trackingItemInformation
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun initViews() {
+        binding?.recyclerView?.apply {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            adapter = TrackingItemsAdapter()
+        }
+    }
+
+    private fun bindView() {
+        binding?.refreshLayout?.setOnRefreshListener {
+            presenter.refresh()
+        }
+        binding?.addTrackingItemButton?.setOnClickListener {
+            findNavController().navigate(R.id.to_add_tracking_item)
+        }
+        binding?.addTrackingItemFloatingActionButton?.setOnClickListener { _ ->
+            findNavController().navigate(R.id.to_add_tracking_item)
+        }
+        (binding?.recyclerView?.adapter as? TrackingItemsAdapter)?.onClickItemListener = { item, information ->
+            findNavController()
+                .navigate(TrackingItemsFragmentDirections.toTrackingHistory(item, information))
+        }
+    }
 }
