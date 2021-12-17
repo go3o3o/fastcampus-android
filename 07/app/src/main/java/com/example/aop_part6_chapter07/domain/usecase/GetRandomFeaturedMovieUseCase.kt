dd83c4a39fb76f5f65d5c20e9ac9429e09bf72e0
yonikim <hoyeon.kim2773@gmail.com>
2021-12-17 09:47:38 +0900

    안드로이드앱 > 영화 평점 서비스

--- 07/app/src/main/java/com/example/aop_part6_chapter07/domain/usecase/GetRandomFeaturedMovieUseCase.kt
+++ 07/app/src/main/java/com/example/aop_part6_chapter07/domain/usecase/GetRandomFeaturedMovieUseCase.kt
@@ -1,4 +1,27 @@
 package com.example.aop_part6_chapter07.domain.usecase
 
-class GetRandomFeaturedMovieUseCase {
+import com.example.aop_part6_chapter07.data.repository.MovieRepository
+import com.example.aop_part6_chapter07.data.repository.ReviewRepository
+import com.example.aop_part6_chapter07.domain.model.FeaturedMovie
+
+class GetRandomFeaturedMovieUseCase(
+    private val movieRepository: MovieRepository,
+    private val reviewRepository: ReviewRepository
+) {
+
+    suspend operator fun invoke(): FeaturedMovie? {
+        val featuredMovies = movieRepository.getAllMovies()
+            .filter { !it.id.isNullOrBlank() }
+            .filter { it.isFeatured == true }
+
+        if (featuredMovies.isNullOrEmpty()) {
+            return null
+        }
+
+        return featuredMovies.random()
+            .let { movie ->
+                val latestReview = reviewRepository.getLatestReview(movie.id!!)
+                FeaturedMovie(movie, latestReview)
+            }
+    }
 }
