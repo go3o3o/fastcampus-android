607269c0a0e376f9e36fc4171259666050c79a59
yonikim <hoyeon.kim2773@gmail.com>
2021-12-18 18:49:17 +0900

    안드로이드앱 > 영화 평점 서비스

---
+++ 07/app/src/main/java/com/example/aop_part6_chapter07/di/AppModule.kt
@@ -0,0 +1,56 @@
+package com.example.aop_part6_chapter07.di
+
+import android.app.Activity
+import com.example.aop_part6_chapter07.data.api.*
+import com.example.aop_part6_chapter07.data.preference.PreferenceManager
+import com.example.aop_part6_chapter07.data.preference.SharedPreferenceManager
+import com.example.aop_part6_chapter07.data.repository.*
+import com.example.aop_part6_chapter07.domain.model.Movie
+import com.example.aop_part6_chapter07.domain.usecase.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val appModule = module {
+    single { Dispatchers.IO }
+}
+
+val dataModule = module {
+    single { Firebase.firestore }
+
+    single<MovieApi> { MovieFirestoreApi(get()) }
+    single<ReviewApi> { ReviewFirestoreApi(get()) }
+    single<UserApi> { UserFirestoreApi(get()) }
+
+    single<MovieRepository> { MovieRepositoryImpl(get(), get()) }
+    single<ReviewRepository> { ReviewRepositoryImpl(get(), get()) }
+    single<UserRepository> { UserRepositoryImpl(get(), get(), get()) }
+
+    single { androidContext().getSharedPreferences("preference", Activity.MODE_PRIVATE) }
+    single<PreferenceManager> { SharedPreferenceManager(get()) }
+}
+
+val domainModule = module {
+    factory { GetRandomFeaturedMovieUseCase(get(), get()) }
+    factory { GetAllMoviesUseCase(get()) }
+    factory { GetAllMovieReviewsUseCase(get(), get()) }
+    factory { GetMyReviewedMoviesUseCase(get(), get(), get()) }
+    factory { SubmitReviewUseCase(get(), get()) }
+    factory { DeleteReviewUseCase(get()) }
+}
+
+val presenterModule = module {
+    scope<HomeFragment> {
+        scoped<HomeContract.Presenter> { HomePresenter(getSource(), get(), get()) }
+    }
+    scope<MovieReviewsFragment> {
+        scoped<MovieReviewsContract.Presenter> { (movie: Movie) ->
+            MovieReviewsPresenter(movie, getSource(), get(), get(), get())
+        }
+    }
+    scope<MyPageFragment> {
+        scoped<MyPageContract.Presenter> { MyPagePresenter(getSource(), get()) }
+    }
+}
