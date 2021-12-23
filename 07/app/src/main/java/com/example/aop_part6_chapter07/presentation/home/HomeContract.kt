5be1f5930b2cf6047ebf79ae06ca071e97f58cc9
yonikim <hoyeon.kim2773@gmail.com>
2021-12-23 14:08:17 +0900

    안드로이드앱 > 영화 평점 서비스

---
+++ 07/app/src/main/java/com/example/aop_part6_chapter07/presentation/home/HomeContract.kt
@@ -0,0 +1,24 @@
+package com.example.aop_part6_chapter07.presentation.home
+
+import com.example.aop_part6_chapter07.domain.model.FeaturedMovie
+import com.example.aop_part6_chapter07.domain.model.Movie
+import com.example.aop_part6_chapter07.presentation.BasePresenter
+import com.example.aop_part6_chapter07.presentation.BaseView
+
+interface HomeContract {
+    interface View : BaseView<Presenter> {
+
+        fun showLoadingIndicator()
+
+        fun hideLoadingIndicator()
+
+        fun showErrorDescription(message: String)
+
+        fun showMovies(
+            featuredMovie: FeaturedMovie?,
+            movies: List<Movie>
+        )
+    }
+
+    interface Presenter : BasePresenter
+}
