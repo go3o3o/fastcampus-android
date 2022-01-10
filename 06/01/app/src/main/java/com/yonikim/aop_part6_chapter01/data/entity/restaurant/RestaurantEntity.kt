6046f12d38fd213f1e500553663ef493500c0860
yonikim <hoyeon.kim2773@gmail.com>
2022-01-10 21:13:18 +0900

    안드로이드앱 > 배달앱

---
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/data/entity/restaurant/RestaurantEntity.kt
@@ -0,0 +1,23 @@
+package com.yonikim.aop_part6_chapter01.data.entity.restaurant
+
+import android.os.Parcelable
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.yonikim.aop_part6_chapter01.data.entity.Entity
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@androidx.room.Entity
+@TypeConverters(RoomTypeConverters::class)
+data class RestaurantEntity(
+    override val id: Long,
+    val restaurantInfoId: Long,
+    val restaurantCategory: RestaurantCategory,
+    @PrimaryKey val restaurantTitle: String,
+    val restaurantImageUrl: String,
+    val grade: Float,
+    val reviewCount: Int,
+    val deliveryTimeRange: Pair<Int, Int>,
+    val deliveryTipRange: Pair<Int, Int>,
+    val restaurantTelNumber: String?
+): Entity, Parcelable
