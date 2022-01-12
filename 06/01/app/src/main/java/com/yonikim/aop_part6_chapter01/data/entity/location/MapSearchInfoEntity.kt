92f710bbc14de606f468956638a43dd28da6b86c
yonikim <hoyeon.kim2773@gmail.com>
2022-01-12 15:05:35 +0900

    안드로이드앱 > 배달앱

---
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/data/entity/location/MapSearchInfoEntity.kt
@@ -0,0 +1,11 @@
+package com.yonikim.aop_part6_chapter01.data.entity.location
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class MapSearchInfoEntity(
+    val fullAddress: String,
+    val name: String,
+    val locationLatLng: LocationLatLngEntity
+) : Parcelable
