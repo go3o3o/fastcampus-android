7e337e9463f48e46209138fcc37fc6a8351b74de
yonikim <hoyeon.kim2773@gmail.com>
2022-01-16 09:47:41 +0900

    안드로이드앱 > 배달앱

--- 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/data/db/ApplicationDatabase.kt
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/data/db/ApplicationDatabase.kt
@@ -1,6 +1,9 @@
 package com.yonikim.aop_part6_chapter01.data.db
 
 import androidx.room.RoomDatabase
+import com.yonikim.aop_part6_chapter01.data.db.dao.FoodMenuBasketDao
+import com.yonikim.aop_part6_chapter01.data.db.dao.LocationDao
+import com.yonikim.aop_part6_chapter01.data.db.dao.RestaurantDao
 
 abstract class ApplicationDatabase : RoomDatabase() {
     companion object {
