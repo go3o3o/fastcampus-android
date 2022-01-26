1eafc455c47597cbb12f83a987b0a7a33e2195d7
yonikim <hoyeon.kim2773@gmail.com>
2022-01-26 19:35:16 +0900

    안드로이드앱 > 배달앱

---
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/model/CellType.kt
@@ -0,0 +1,10 @@
+package com.yonikim.aop_part6_chapter01.model
+
+enum class CellType {
+    RESTAURANT_CELL,
+    LIKE_RESTAURANT_CELL,
+    FOOD_CELL,
+    ORDER_FOOD_CELL,
+    REVIEW_CELL,
+    ORDER_CELL
+}
