d575166a87e59010c6e139f703673a8789fee778
yonikim <hoyeon.kim2773@gmail.com>
2022-02-01 19:47:29 +0900

    안드로이드앱 > 배달앱

---
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/util/mapper/ModelViewHolderMapper.kt
@@ -0,0 +1,62 @@
+package com.yonikim.aop_part6_chapter01.util.mapper
+
+import android.content.res.loader.ResourcesProvider
+import android.view.ViewGroup
+import com.yonikim.aop_part6_chapter01.model.CellType
+import com.yonikim.aop_part6_chapter01.model.Model
+
+object ModelViewHolderMapper {
+
+    @Suppress("UNCHECKED_CAST")
+    fun <M: Model> map(
+        parent: ViewGroup,
+        type: CellType,
+        viewModel: BaseViewModel,
+        resourcesProvider: ResourcesProvider
+    ): ModelViewHolder<M> {
+        val inflater = LayoutInflater.from(parent.context)
+        val viewHolder = when (type) {
+            CellType.RESTAURANT_CELL ->
+                RestaurantViewHolder(
+                    ViewholderRestaurantBinding.inflate(inflater, parent, false),
+                    viewModel,
+                    resourcesProvider
+                )
+            CellType.LIKE_RESTAURANT_CELL ->
+                LikeRestaurantViewHolder(
+                    ViewholderLikeRestaurantBinding.inflate(inflater, parent, false),
+                    viewModel,
+                    resourcesProvider
+                )
+            CellType.FOOD_CELL ->
+                FoodMenuViewHolder(
+                    ViewholderFoodMenuBinding.inflate(inflater, parent, false),
+                    viewModel,
+                    resourcesProvider
+                )
+            CellType.ORDER_FOOD_CELL ->
+                OrderMenuViewHolder(
+                    ViewholderOrderMenuBinding.inflate(inflater, parent, false),
+                    viewModel,
+                    resourcesProvider
+                )
+            CellType.REVIEW_CELL -> {
+                RestaurantReviewViewHolder(
+                    ViewholderRestaurantReviewBinding.inflate(inflater, parent, false),
+                    viewModel,
+                    resourcesProvider
+                )
+            }
+            CellType.ORDER_CELL -> {
+                OrderViewHolder(
+                    ViewholderOrderBinding.inflate(inflater, parent, false),
+                    viewModel,
+                    resourcesProvider
+                )
+            }
+        }
+
+        return viewHolder as ModelViewHolder<M>
+    }
+
+}
