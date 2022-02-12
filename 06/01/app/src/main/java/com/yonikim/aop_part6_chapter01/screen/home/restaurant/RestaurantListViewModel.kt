509ccc765b5359d5d96442a1dceca208111a2846
yonikim <hoyeon.kim2773@gmail.com>
2022-02-12 20:51:48 +0900

    안드로이드앱 > 배달앱

---
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/screen/home/restaurant/RestaurantListViewModel.kt
@@ -0,0 +1,65 @@
+package com.yonikim.aop_part6_chapter01.screen.home.restaurant
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.yonikim.aop_part6_chapter01.data.entity.location.LocationLatLngEntity
+import com.yonikim.aop_part6_chapter01.data.repository.restaurant.RestaurantRepository
+import com.yonikim.aop_part6_chapter01.model.restaurant.RestaurantModel
+import com.yonikim.aop_part6_chapter01.screen.base.BaseViewModel
+import kotlinx.coroutines.Job
+
+class RestaurantListViewModel(
+    private val restaurantCategory: RestaurantCategory,
+    private var locationLatLngEntity: LocationLatLngEntity,
+    private val restaurantRepository: RestaurantRepository,
+    private var restaurantFilterOrder: RestautantFilterOrder = RestautantFilterOrder.DEFAULT
+) : BaseViewModel() {
+
+    private var _restaurantListLiveData = MutableLiveData<List<RestaurantModel>>()
+    val restaurantListLiveData: LiveData<List<RestaurantModel>>
+        get() = _restaurantListLiveData
+
+    override fun fetchData(): Job = viewModelScope.launch {
+        val restaurantList = restaurantRepository.getList(restaurantCategory, locationLatLngEntity)
+        val sortedList = when (restaurantFilterOrder) {
+            RestautantFilterOrder.DEFAULT -> {
+                restaurantList
+            }
+            RestautantFilterOrder.LOW_DELIVERY_TIP -> {
+                restaurantList.sortedBy { it.deliveryTipRange.first }
+            }
+            RestautantFilterOrder.FAST_DELIVERY -> {
+                restaurantList.sortedBy { it.deliveryTimeRange.first }
+            }
+            RestautantFilterOrder.TOP_RATE -> {
+                restaurantList.sortedByDescending { it.grade }
+            }
+        }
+        _restaurantListLiveData.value = sortedList.map {
+            RestaurantModel(
+                id = it.id,
+                restaurantInfoId = it.restaurantInfoId,
+                restaurantCategory = it.restaurantCategory,
+                restaurantTitle = it.restaurantTitle,
+                restaurantImageUrl = it.restaurantImageUrl,
+                grade = it.grade,
+                reviewCount = it.reviewCount,
+                deliveryTimeRange = it.deliveryTimeRange,
+                deliveryTipRange = it.deliveryTipRange,
+                restaurantTelNumber = it.restaurantTelNumber
+            )
+        }
+    }
+
+    fun setLocationLatLng(locationLatLngEntity: LocationLatLngEntity) {
+        this.locationLatLngEntity = locationLatLngEntity
+        fetchData()
+    }
+
+    fun setRestaurantFilterOrder(order: RestautantFilterOrder) {
+        this.restaurantFilterOrder = order
+        fetchData()
+    }
+
+}
