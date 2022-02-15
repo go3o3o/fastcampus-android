b3441b4f9ac0862404592ef0fadb66ed74787471
yonikim <hoyeon.kim2773@gmail.com>
2022-02-15 22:12:04 +0900

    안드로이드앱 > 배달앱

---
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/screen/order/OrderMenuState.kt
@@ -0,0 +1,21 @@
+package com.yonikim.aop_part6_chapter01.screen.order
+
+import androidx.annotation.StringRes
+import com.yonikim.aop_part6_chapter01.model.restaurant.FoodModel
+
+sealed class OrderMenuState {
+    object Uninitialized: OrderMenuState()
+
+    object Loading: OrderMenuState()
+
+    data class Success(
+        val restaurantFoodModelList: List<FoodModel>? = null
+    ): OrderMenuState()
+
+    object Order: OrderMenuState()
+
+    data class Error(
+        @StringRes val messageId: Int,
+        val e: Throwable
+    ): OrderMenuState()
+}
