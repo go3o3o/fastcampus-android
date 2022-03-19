497cfffe651cf5ef06cfc3ea59f80c9d4b0070b1
yonikim <hoyeon.kim2773@gmail.com>
2022-03-19 13:14:37 +0900

    안드로이드앱 > 배달앱

---
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/widget/adapter/viewholder/order/OrderMenuViewHolder.kt
@@ -0,0 +1,40 @@
+package com.yonikim.aop_part6_chapter01.widget.adapter.viewholder.order
+
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.yonikim.aop_part6_chapter01.R
+import com.yonikim.aop_part6_chapter01.databinding.ViewholderOrderMenuBinding
+import com.yonikim.aop_part6_chapter01.extensions.load
+import com.yonikim.aop_part6_chapter01.model.restaurant.FoodModel
+import com.yonikim.aop_part6_chapter01.screen.base.BaseViewModel
+import com.yonikim.aop_part6_chapter01.util.provider.ResourcesProvider
+import com.yonikim.aop_part6_chapter01.widget.adapter.viewholder.ModelViewHolder
+
+class OrderMenuViewHolder(
+    private val binding: ViewholderOrderMenuBinding,
+    viewModel: BaseViewModel,
+    resourcesProvider: ResourcesProvider
+): ModelViewHolder<FoodModel>(binding, viewModel, resourcesProvider) {
+
+    override fun reset() = with(binding) {
+        foodImage.clear()
+    }
+
+    override fun bindData(model: FoodModel) {
+        super.bindData(model)
+        with(binding) {
+            foodImage.load(model.imageUrl, 24f, CenterCrop())
+            foodTitleText.text = model.title
+            foodDescriptionText.text = model.description
+            priceText.text = resourcesProvider.getString(R.string.price, model.price)
+        }
+    }
+
+    override fun bindViews(model: FoodModel, adapterListener: AdapterListener) {
+        if (adapterListener is OrderMenuListListener) {
+            binding.root.setOnClickListener {
+                adapterListener.onRemoveItem(model)
+            }
+        }
+    }
+
+}
