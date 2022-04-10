0d3e91959e23a374bd8a1746324d9368e645e7cb
yonikim <hoyeon.kim2773@gmail.com>
2022-04-10 11:57:58 +0900

    안드로이드앱 > 배달앱

--- 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/screen/review/gallery/GalleryViewModel.kt
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/screen/review/gallery/GalleryViewModel.kt
@@ -1,5 +1,10 @@
 package com.yonikim.aop_part6_chapter01.screen.review.gallery
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
 class GalleryViewModel(
     private val galleryPhotoRepository: GalleryPhotoRepository
 ) : ViewModel() {
