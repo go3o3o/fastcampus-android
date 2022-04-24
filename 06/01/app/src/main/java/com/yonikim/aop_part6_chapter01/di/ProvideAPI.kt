f32c21f46b9a5b8a3976dc3ab53dce3bc2fad6f1
yonikim <hoyeon.kim2773@gmail.com>
2022-04-24 14:48:16 +0900

    안드로이드앱 > 배달앱

--- 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/di/ProvideAPI.kt
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/di/ProvideAPI.kt
@@ -1,5 +1,17 @@
 package com.yonikim.aop_part6_chapter01.di
 
+import androidx.viewbinding.BuildConfig
+import com.yonikim.aop_part6_chapter01.data.network.FoodApiService
+import com.yonikim.aop_part6_chapter01.data.network.MapApiService
+import com.yonikim.aop_part6_chapter01.data.url.TmapUrl.FOOD_URL
+import com.yonikim.aop_part6_chapter01.data.url.TmapUrl.TMAP_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
 fun provideMapApiService(retrofit: Retrofit): MapApiService {
     return retrofit.create(MapApiService::class.java)
 }
