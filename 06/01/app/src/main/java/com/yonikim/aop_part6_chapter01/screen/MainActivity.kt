9b9e3d4f01ec857e88eae4097ed59e80c19a9f4f
yonikim <hoyeon.kim2773@gmail.com>
2022-05-20 14:07:31 +0900

    안드로이드앱 > 배달앱

--- 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/screen/MainActivity.kt
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/screen/MainActivity.kt
@@ -2,11 +2,79 @@ package com.yonikim.aop_part6_chapter01.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.yonikim.aop_part6_chapter01.R
+import com.yonikim.aop_part6_chapter01.databinding.ActivityMainBinding
+import com.yonikim.aop_part6_chapter01.screen.base.BaseActivity
+import com.yonikim.aop_part6_chapter01.util.event.MenuChangeEventBus
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.java.KoinJavaComponent.inject
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override val viewModel by viewModel<MainViewModel>()
+
+    override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+    private val menuChangeEventBus by inject<MenuChangeEventBus>()
+
+    override fun initState() {
+        super.initState()
+        lifecycleScope.launch {
+            menuChangeEventBus.changeMenu(MainTabMenu.HOME)
+        }
     }
+
+    override fun initViews() = with(binding) {
+        bottomNav.setOnNavigationItemSelectedListener(this@MainActivity)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_home -> {
+                showFragment(HomeFragment.newInstance(), HomeFragment.TAG)
+                true
+            }
+            R.id.menu_like -> {
+                showFragment(RestaurantLikeListFragment.newInstance(), RestaurantLikeListFragment.TAG)
+                true
+            }
+            R.id.menu_my -> {
+                showFragment(MyFragment.newInstance(), MyFragment.TAG)
+                true
+            }
+            else -> false
+        }
+    }
+
+    fun goToTab(mainTabMenu: MainTabMenu) {
+        binding.bottomNav.selectedItemId = mainTabMenu.menuId
+    }
+
+    private fun showFragment(fragment: Fragment, tag: String) {
+        val findFragment = supportFragmentManager.findFragmentByTag(tag)
+        supportFragmentManager.fragments.forEach { fm ->
+            supportFragmentManager.beginTransaction().hide(fm).commitAllowingStateLoss()
+        }
+        findFragment?.let {
+            supportFragmentManager.beginTransaction().show(it).commitAllowingStateLoss()
+        } ?: kotlin.run {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment, tag)
+                .commitAllowingStateLoss()
+        }
+    }
+
+    override fun observeData() {
+        lifecycleScope.launch {
+            menuChangeEventBus.mainTabMenuFlow.collect {
+                goToTab(it)
+            }
+        }
+    }
+
+}
+
+enum class MainTabMenu(@IdRes val menuId: Int) {
+    HOME(R.id.menu_home), MY(R.id.menu_my)
 }
