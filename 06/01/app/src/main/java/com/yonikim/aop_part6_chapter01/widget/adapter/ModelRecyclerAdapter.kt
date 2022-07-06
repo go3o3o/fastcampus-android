21b9bd29878313a0de90a1837775abf2fe8287b4
yonikim <hoyeon.kim2773@gmail.com>
2022-07-06 18:02:09 +0900

    안드로이드앱 > 배달앱

--- 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/widget/adapter/ModelRecyclerAdapter.kt
+++ 06/01/app/src/main/java/com/yonikim/aop_part6_chapter01/widget/adapter/ModelRecyclerAdapter.kt
@@ -1,6 +1,5 @@
 package com.yonikim.aop_part6_chapter01.widget.adapter
 
-import android.content.res.loader.ResourcesProvider
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.yonikim.aop_part6_chapter01.Part6Chapter01Application
@@ -9,6 +8,9 @@ import com.yonikim.aop_part6_chapter01.model.Model
 import com.yonikim.aop_part6_chapter01.screen.base.BaseViewModel
 import com.yonikim.aop_part6_chapter01.util.mapper.ModelViewHolderMapper
 import com.yonikim.aop_part6_chapter01.util.provider.DefaultResourcesProvider
+import com.yonikim.aop_part6_chapter01.util.provider.ResourcesProvider
+import com.yonikim.aop_part6_chapter01.widget.adapter.listener.AdapterListener
+import com.yonikim.aop_part6_chapter01.widget.adapter.viewholder.ModelViewHolder
 
 class ModelRecyclerAdapter<M : Model, VM: BaseViewModel>(
     private var modelList: List<Model>,
